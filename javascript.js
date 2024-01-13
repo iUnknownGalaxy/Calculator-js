d83a1433725e668a29760334af224ab37ab0695b
iUnknownGalaxy <18318229+iUnknownGalaxy@users.noreply.github.com>
2024-01-13 11:13:11 -0500

    Add function to choose an operation on two numbers

--- javascript.js
+++ javascript.js
@@ -14,4 +14,26 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-console.log(multiply(3,7));
+let num1;
+let num2;
+let operator;
+
+function operate(num1, num2, operator) {
+    if(operator === "+") {
+        return add(num1, num2);
+    } else if(operator === "-") {
+        return subtract(num1, num2);
+    } else if(operator === "*") {
+        return multiply(num1, num2);
+    } else if(operator === "/") {
+        return divide(num1, num2);
+    } else {
+        throw new Error("Not a valid operator!");
+    }
+}
+
+try {
+    console.log(operate(3, 4, "-"));
+} catch(e) {
+    console.error(e);
+}
