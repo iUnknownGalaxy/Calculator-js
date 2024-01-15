c0e57742ddba1cb0fdb7f1d6fe9ecc559b07a5d1
iUnknownGalaxy <18318229+iUnknownGalaxy@users.noreply.github.com>
2024-01-15 12:19:22 -0500

    feat: Add delete button and adjust colors

--- javascript.js
+++ javascript.js
@@ -32,27 +32,38 @@ function createNumbers() {
     for(let i = 9; i >= 0; i--) {
         const numberButton = document.createElement("button");
         numberButton.textContent = `${i}`;
-        numberButton.style.cssText = numberStyle;
+        numberButton.style.cssText = buttonStyle;
         numbers.appendChild(numberButton);
         if(i === 1) {
             const dotButton = document.createElement("button");
             dotButton.textContent = ".";
-            dotButton.style.cssText = numberStyle;
+            dotButton.style.cssText = buttonStyle;
             numbers.appendChild(dotButton);
         }
     }
-    const emptyDiv = document.createElement("div");
-    emptyDiv.style.cssText = numberStyle;
-    numbers.appendChild(emptyDiv);
+    const negativeButton = document.createElement("button");
+    negativeButton.style.cssText = buttonStyle;
+    negativeButton.textContent = "(-/+)";
+    numbers.appendChild(negativeButton);
 }
 
 function createOperators() {
-    let operatorArray = ["*", "-", "+", "="]
+    let operatorArray = ["*", "-", "+", "="];
     for(let i = 0; i < operatorArray.length; i++) {
         const operatorButton = document.createElement("button");
         operatorButton.textContent = operatorArray[i];
-        operatorButton.style.cssText = numberStyle;
+        operatorButton.style.cssText = buttonStyle;
         operators.appendChild(operatorButton);
+    }
+}
+
+function createAdjusters() {
+    let adjusterArray = ["C", "(", ")", "/"];
+    for(let i = 0; i < adjusterArray.length; i++) {
+        const adjusterButton = document.createElement("button");
+        adjusterButton.textContent = adjusterArray[i];
+        adjusterButton.style.cssText = buttonStyle;
+        adjusters.appendChild(adjusterButton);
     }
 }
 
@@ -63,13 +74,14 @@ const calculator = document.getElementById("calculator");
 const display = document.getElementById("display");
 const numbers = document.getElementById("numbers");
 const operators = document.getElementById("operators");
+const adjusters = document.getElementById("adjusters");
+const deleteButton = document.getElementById("delete");
 const LAST_DIGIT = 9;
-const CALCULATOR_HEIGHT = 600;
 const CALCULATOR_WIDTH = 400;
 
 const calculatorStyle = `
     width: ${CALCULATOR_WIDTH}px;
-    height: ${CALCULATOR_HEIGHT}px;
+    height: auto;
     display: flex;
     justify-content:center;
     flex-direction: column;
@@ -77,15 +89,41 @@ const calculatorStyle = `
     border: solid black 3px;
     border-radius: 8px;
 `;
-const numberStyle = `
-    width: ${CALCULATOR_WIDTH/4}px;
-    height: ${CALCULATOR_WIDTH/4}px;
+const buttonStyle = `
+    width: ${(CALCULATOR_WIDTH-24)/4}px;
+    height: ${(CALCULATOR_WIDTH-24)/4}px;
     border-radius: 8px;
-    margin: 0px;
+    margin: 3px;
     font-size: 20px;
+    background-color: #e8a87c;
 `;
+
+const displayStyle = `
+    display: flex;
+    justify-content: end;
+    align-items: center;
+    width: ${CALCULATOR_WIDTH}px;
+    height: 100px;
+    font-size: 50px;
+    font-weight: bold;
+    border: solid 2px;
+`;
+
+const deleteButtonStyle = `
+    width: ${(CALCULATOR_WIDTH-24)/4}px;
+    height: ${(CALCULATOR_WIDTH-24)/8}px;
+    border-radius: 8px;
+    margin: 3px;
+    font-size: 20px;
+    background-color: #e8a87c;
+`;
+
 calculator.style.cssText = calculatorStyle;
+display.style.cssText = displayStyle;
+deleteButton.style.cssText = deleteButtonStyle;
+deleteButton.textContent = "←";
 
 createNumbers();
 createOperators();
+createAdjusters();
 
